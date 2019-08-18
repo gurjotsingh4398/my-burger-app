3730ca9a72a7a321ab828a0618f6b204d9753568
Gurjot Singh <34470603+gurjotsingh4398@users.noreply.github.com>
2019-08-18 15:30:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers =
-  process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+const composeEnhancers = compose;
+//   process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
